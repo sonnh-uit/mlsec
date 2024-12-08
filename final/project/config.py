cdacec459079433b720779a5bbf330f9265efd9b
sonnh <sonnh.uit@gmail.com>
2024-12-08 10:37:09 +0700

    update new version of model, update main script to run model

--- final/project/config.py
+++ final/project/config.py
@@ -1,7 +1,7 @@
 # author:  sonnh
 # email:   sonnh.tech@gmail.com
 # created: 2024-12-07 00:40:40
-# updated: 2024-12-07 00:53:46
+# updated: 2024-12-07 11:09:28
 
 import argparse
 
@@ -11,9 +11,3 @@ def build_args():
     parser.add_argument("--model", type=str, default="xgboost",help="`xgboost`")
     args = parser.parse_args()
     return args
-
-def main():
-    print('Hello World!')
-
-if __name__ == "__main__":
-    main()
--- final/project/main.py
+++ final/project/main.py
@@ -1,7 +1,7 @@
 # author:  sonnh
 # email:   sonnh.tech@gmail.com
 # created: 2024-12-06 19:19:28
-# updated: 2024-12-07 01:16:46
+# updated: 2024-12-08 10:35:50
 
 from config import build_args
 import pandas as pd
@@ -9,31 +9,50 @@ import pickle
 from sklearn.metrics import f1_score, accuracy_score
 from sklearn.preprocessing import  LabelEncoder
 from xgboost import XGBClassifier
+import json 
 
-def predict(model, dataset):
-    case = dataset.sample()
-    X_val = dataset.drop(columns=['Label'])
-    y_val = dataset['Label']
 
+def boost_predict(dataset):
+    with open(f"models/xgboost/model_summary.json", 'r') as file:
+        metrics_ = json.load(file)
+    data = dataset.drop(columns=metrics_['droped_column'][0])
+    
+    model_ = XGBClassifier()
+    model_.load_model(f"models/xgboost/model.xgb")
+    X_val = data.drop(columns=['Label'])
     label_encoder = LabelEncoder()
-    y_val = label_encoder.fit_transform(y_val)
+    y_val = label_encoder.fit_transform(data['Label'])
+    y_pred = model_.predict(X_val)
+    f1 = f1_score(y_val, y_pred, average='weighted')
+    print(f"F1 score is: {f1}")
 
-    # y_pred = model.predict(X_val)
-    # f1 = f1_score(y_val, y_pred, average='weighted')
-    # print(f"F1 score is: {f1}")
-
-    print(case)
-    model.predict(pd.DataFrame(case.drop(columns=['Label'])))
+def DCT_predict(dataset):
+    
+    with open(f"models/decision-tree/model_summary.json", 'r') as file:
+        metrics_ = json.load(file)
+    with open('models/decision-tree/model.pkl', "rb") as file:
+        model_ = pickle.load(file)
+    data = dataset.drop(columns=metrics_['droped_column'][0])
+    X_val = data.drop(columns=['Label'])
+    y_val = data['Label']
+    y_pred = model_.predict(X_val)
+    f1 = f1_score(y_val, y_pred, average='weighted')
+    print(f"F1 score is: {f1}")
+    
 
 def main(main_args):
     val_dataset_ = pd.read_csv(main_args.dataset)
     model_name = main_args.model
     if model_name == 'xgboost':
-        model_ = XGBClassifier()
-        model_.load_model(f"models/{model_name}/model.xgb")
-        predict(model_, val_dataset_)
+        print("Run prediction with XGBoost...")
+        boost_predict(val_dataset_)
+    elif model_name == 'decision-tree':
+        print("Run prediction with Decision Tree...")
+        DCT_predict(val_dataset_)
+    else:
+        print('Model not support!')
+        exit(1)
 
-    print('Hello World!')
 
 if __name__ == "__main__":
     args = build_args()
